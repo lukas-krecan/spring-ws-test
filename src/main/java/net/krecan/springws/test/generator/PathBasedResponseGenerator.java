5cb6a72f4b44f491355c8f6fd5e75ed3878bb22e
lkrecan <lkrecan@aff05851-953e-0410-954f-fdc4ef97531c>
2009-07-05 15:18:57 +0000

    XPath working

--- src/main/java/net/krecan/springws/test/generator/PathBasedResponseGenerator.java
+++ src/main/java/net/krecan/springws/test/generator/PathBasedResponseGenerator.java
@@ -8,17 +8,23 @@ import net.krecan.springws.test.util.XmlUtil;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.WebServiceMessageFactory;
 import org.springframework.xml.xpath.XPathExpression;
 import org.w3c.dom.Document;
 
+
+
 //TODO refactor, logging
-public class ResourceBasedResponseGenerator implements ResponseGenerator {
+public class PathBasedResponseGenerator implements ResponseGenerator, ResourceLoaderAware {
 	
 	private XPathExpression resourceXPathExpression;
+	
+	private ResourceLoader resourceLoader = new DefaultResourceLoader();
 	
 	protected final Log logger = LogFactory.getLog(getClass());
 	
@@ -28,12 +34,12 @@ public class ResourceBasedResponseGenerator implements ResponseGenerator {
 		{
 			String resourcePath = resourceXPathExpression.evaluateAsString(loadDocument(request));
 			logger.debug("Looking for classpath resource \""+resourcePath+"\"");
-			resultResource = new ClassPathResource(resourcePath);
+			resultResource = resourceLoader.getResource(resourcePath);
 		}
 		if (resultResource==null || !resultResource.exists())
 		{
 			logger.debug("Resource not found, using default.");
-			resultResource = new ClassPathResource("mock-responses/test/default-response.xml");
+			resultResource = resourceLoader.getResource("mock-responses/test/default-response.xml");
 		}
 		return messageFactory.createWebServiceMessage(resultResource.getInputStream());
 	}
@@ -49,6 +55,14 @@ public class ResourceBasedResponseGenerator implements ResponseGenerator {
 	protected Document loadDocument(WebServiceMessage message)
 	{
 		return XmlUtil.loadDocument(message);
+	}
+
+	public ResourceLoader getResourceLoader() {
+		return resourceLoader;
+	}
+
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
 	}
 
 }
--- src/test/java/net/krecan/springws/test/generator/PathBasedResponseGeneratorTest.java
+++ src/test/java/net/krecan/springws/test/generator/PathBasedResponseGeneratorTest.java
@@ -1,8 +1,8 @@
 package net.krecan.springws.test.generator;
 
+import static net.krecan.springws.test.util.XmlUtil.loadDocument;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static net.krecan.springws.test.util.XmlUtil.*;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -19,14 +19,13 @@ import org.springframework.xml.xpath.XPathExpressionFactory;
 import org.w3c.dom.Document;
 
 
-public class ResourceBasedResponseGeneratorTest extends AbstractMessageTest{
+public class PathBasedResponseGeneratorTest extends AbstractMessageTest{
 	
-	private ResourceBasedResponseGenerator generator;
-	public ResourceBasedResponseGeneratorTest() {
-		generator = new ResourceBasedResponseGenerator();
-		XPathExpression resourceXpathExpression = XPathExpressionFactory.createXPathExpression("//ns:text", Collections.singletonMap("ns", "http://www.example.org/schema"));
+	private PathBasedResponseGenerator generator;
+	public PathBasedResponseGeneratorTest() {
+		generator = new PathBasedResponseGenerator();
+		XPathExpression resourceXpathExpression = XPathExpressionFactory.createXPathExpression("concat('mock-responses/test/',//ns:text,'-response.xml')", Collections.singletonMap("ns", "http://www.example.org/schema"));
 		generator.setResourceXPathExpression(resourceXpathExpression);
-
 	}
 	@Test
 	public void testDefaultResponse() throws IOException
